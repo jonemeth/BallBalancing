e9408244b5d33d447d8c1803a6bb689b17e2948d
Jozsef Nemeth <jozsnem@gmail.com>
2022-11-06 20:31:00 +0100

    Fixing in RealEnvironment

--- bblib/environments/RealEnvironment.py
+++ bblib/environments/RealEnvironment.py
@@ -1,29 +1,27 @@
 import time
-import serial
-from PIL import Image
-from adafruit_servokit import ServoKit
+from typing import Optional
 
 import numpy as np
-from filterpy.kalman import KalmanFilter
+import serial
+from adafruit_servokit import ServoKit
 
-from bblib.defs import EnvironmentConfig, Position, Angle, EnvironmentState, Observation, Action, Speed
+from bblib.defs import EnvironmentConfig, Position, Angle, EnvironmentState, Observation, Action
 from bblib.environments.Environment import Environment, EnvironmentFactory
-from bblib.utils import compute_angle, init_motion_kalman, random_environment_state
+from bblib.utils import compute_angle, random_environment_state
 
 
 class RealEnvironment(Environment):
     def __init__(self, config: EnvironmentConfig, init_env_state: EnvironmentState):
         super().__init__(config, init_env_state)
-        
+
         self.ser = serial.Serial('/dev/ttyACM0', 9600)
         self.kit = ServoKit(channels=16)
-        self.kalman: Optional[KalmanFilter] = None
 
         self.center = Angle(85.1, 79.0)
         self.set_servo()
         self.observe_position()
         time.sleep(2.0)
-        
+
     def set_servo(self):
         angle = self.observe_angle()
         x = self.center.x + angle.x * 180 / 3.141592
@@ -31,18 +29,21 @@ class RealEnvironment(Environment):
         self.kit.servo[0].angle = x
         self.kit.servo[1].angle = y
 
-
     def observe_position(self) -> Position:
         self.ser.reset_input_buffer()
         while True:
+            read_serial = self.ser.readline().decode("utf-8").strip()
+            tokens = read_serial.split(' , ')
+            if 2 != len(tokens):
+                continue
             try:
-                read_serial = self.ser.readline().decode("utf-8").strip()
-                x, y = [int(v) for v in read_serial.split(' , ')]
-                break
-            except:
+                x, y = [int(v) for v in tokens]
+                return Position(x / 1000, y / 1000)
+            except ValueError:
                 pass
-        return Position(x/1000, y/1000)
-          
+
+    def observe_real_position(self) -> Optional[Position]:
+        return None
 
     def observe_angle(self) -> Angle:
         return compute_angle(self.config, self.state)
@@ -54,31 +55,12 @@ class RealEnvironment(Environment):
                                min(self.config.max_rotation.x, self.state.rot.x + action.x))
         self.state.rot.y = max(-self.config.max_rotation.y,
                                min(self.config.max_rotation.y, self.state.rot.y + action.y))
-                               
+
         self.set_servo()
         time.sleep(self.config.d_t)
-        
+
         return self.observe()
-                       
-        
-    def observe(self) -> Observation:
-        observed_pos = self.observe_position()
 
-        if not self.kalman:
-            self.kalman = init_motion_kalman(observed_pos, self.config.d_t)
-        else:
-            self.kalman.predict()
-            self.kalman.update([observed_pos.x, observed_pos.y])
-
-        estimated_pos = Position(float(self.kalman.x[0]), float(self.kalman.x[1]))
-        estimated_speed = Speed(float(self.kalman.x[2]), float(self.kalman.x[3]))
-
-        angle = self.observe_angle()
-        action = self.actions[-1] if 1 <= len(self.actions) else None
-
-        return Observation(estimated_pos, estimated_speed, angle, action, observed_pos,
-                           None, self._compute_reward(observed_pos), False)
-    
     def render(self, observation: Observation) -> np.ndarray:
         img = super().render(observation)
         return img
@@ -86,10 +68,8 @@ class RealEnvironment(Environment):
 
 class RealEnvironmentFactory(EnvironmentFactory):
     def __init__(self, env_config: EnvironmentConfig):
-                     
         super().__init__(env_config)
-
 
     def create(self) -> Environment:
         return RealEnvironment(self.env_config,
-                                  random_environment_state())
+                               random_environment_state())
