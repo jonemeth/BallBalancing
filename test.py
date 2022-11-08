88ffb8fca94f2b25d94e3e8ebc39ca03f77439c9
Jozsef Nemeth <jozsnem@gmail.com>
2022-11-08 20:19:35 +0100

    Removing commented code

--- test.py
+++ test.py
@@ -44,28 +44,6 @@ def main():
         frames[0].save(gif_filename, format='GIF', append_images=frames[1:],
                        save_all=True, duration=env_factory.get_env_config().d_t * 1000, loop=0)
 
-    # for i in range(num_episodes):
-    #     env = env_factory.create()
-    #
-    #     episode = run_episode(env, agent, True)
-    #     avg_reward = sum([observation.reward for observation in episode]) / len(episode)
-    #     print(i, avg_reward, agent.epsilon_scheduler.get_epsilon())
-    #
-    #     agent.train()
-    #
-    #     if (i + 1) % 100 == 0 or i >= num_episodes - 10:
-    #         env = env_factory.create()
-    #
-    #         episode = run_episode(env, agent, False)
-    #
-    #         frames = []
-    #         for observation in episode:
-    #             frames.append(Image.fromarray(env.render(observation)))
-    #
-    #         frames[0].save(f"out{i + 1:06d}.gif", format='GIF', append_images=frames[1:],
-    #                        save_all=True, duration=env_factory.get_env_config().d_t * 1000, loop=0)
-
-
     print("Bye!")
 
 
