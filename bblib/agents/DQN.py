8458184d75086195ae074f2ce5d158b35eec5b88
Jozsef Nemeth <jozsnem@gmail.com>
2022-11-08 23:24:45 +0100

    Small adjustments

--- bblib/agents/DQN.py
+++ bblib/agents/DQN.py
@@ -58,7 +58,7 @@ class DQN(Agent):
         super().__init__(action_counts)
         self.env_config = env_config
 
-        self.network = DefaultNetwork(8, [200, 200], self.action_counts, torch.nn.ReLU)
+        self.network = DefaultNetwork(8, [200, 200], self.action_counts, torch.nn.GELU)
         self.epsilon_scheduler = epsilon_scheduler
 
         self.experience_dataset = ExperienceDataset(episodes_in_memory*env_config.get_episode_steps())
--- train.py
+++ train.py
@@ -51,7 +51,7 @@ def main():
 
         agent.train()
 
-        if (i + 1) % 100 == 0 or i >= num_episodes - 10:
+        if (i + 1) % 100 == 0 or i >= num_episodes - 5:
             env = env_factory.create()
 
             episode = run_episode(env, agent, False)
