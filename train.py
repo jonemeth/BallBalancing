e7766f103af2f8e984d06f824ef004b588f406b6
Jozsef Nemeth <jozsnem@gmail.com>
2022-12-01 19:03:31 +0100

    Logging to file

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from utils.config import load_config
 
 MODELS_FOLDER = "models"
 GIFS_FOLDER = "gifs"
+LOG_FILENAME = "log.txt"
 
 KEY_ENVIRONMENT_FACTORY = "environmentFactory"
 KEY_AGENT = "agent"
@@ -19,6 +20,15 @@ KEY_NUM_EPISODES = "num_episodes"
 KEY_SAVE_FOLDER_PREFIX = "save_folder_prefix"
 KEY_TRAIN_EPISODE_SECS = "train_episode_secs"
 KEY_TEST_EPISODE_SECS = "test_episode_secs"
+
+
+class Logger:
+    def __init__(self, filename: Path):
+        self.fp = open(filename, "w+t")
+
+    def log(self, msg: str):
+        print(msg, flush=True)
+        self.fp.write(msg+"\n")
 
 
 def main():
@@ -43,7 +53,7 @@ def main():
 
     save_folder_prefix = config[KEY_SAVE_FOLDER_PREFIX]
     save_folder_suffix = datetime.now().strftime('%y%m%d%H%M%S')
-    save_folder = Path(SAVES_ROOT) / (save_folder_prefix+"_"+save_folder_suffix)
+    save_folder = Path(SAVES_ROOT) / (save_folder_prefix + "_" + save_folder_suffix)
     save_folder.mkdir(parents=True, exist_ok=True)
 
     models_folder = save_folder / MODELS_FOLDER
@@ -51,6 +61,8 @@ def main():
 
     gifs_folder = save_folder / GIFS_FOLDER
     gifs_folder.mkdir(parents=True, exist_ok=True)
+
+    logger = Logger(save_folder / LOG_FILENAME)
 
     running_reward = None
     running_loss = None
@@ -60,14 +72,14 @@ def main():
         episode = run_episode(env, agent, train_episode_secs, True, 1e-8 if args.display else -1.0)
         avg_reward = sum([observation.reward for observation in episode]) / len(episode)
 
-        running_reward = avg_reward if running_reward is None else 0.95*running_reward + 0.05*avg_reward
+        running_reward = avg_reward if running_reward is None else 0.95 * running_reward + 0.05 * avg_reward
         lr = agent.lr_scheduler.get_last_lr()[0]
 
         avg_loss = agent.train()
-        running_loss = avg_loss if running_loss is None else 0.95*running_loss + 0.05*avg_loss
+        running_loss = avg_loss if running_loss is None else 0.95 * running_loss + 0.05 * avg_loss
         running_loss_ = 0.0 if running_loss is None else running_loss
-        print(f"it: {i:4d}, avg_reward: {avg_reward:.4f}, running_reward: {running_reward:.4f}, " +
-              f"running_loss: {running_loss_:.4f}, epsilon: {agent.epsilon_scheduler.get_epsilon():.4f}, lr: {lr:.6f}")
+        logger.log(f"it: {i:4d}, avg_reward: {avg_reward:.4f}, running_reward: {running_reward:.4f}, " +
+                   f"running_loss: {running_loss_:.4f}, epsilon: {agent.epsilon_scheduler.get_epsilon():.4f}, lr: {lr:.6f}")
 
         if (i + 1) % 100 == 0:
             model_file = models_folder / f"model{i + 1:06d}"
@@ -86,7 +98,7 @@ def main():
             frames[0].save(gif_file, format='GIF', append_images=frames[1:],
                            save_all=True, duration=env_factory.get_env_config().d_t * 1000, loop=0)
 
-    print("Bye!")
+    logger.log("Bye!")
 
 
 if __name__ == '__main__':
